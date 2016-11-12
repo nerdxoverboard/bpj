11ae16dbc4aa9380d649eed79ebc788a08326006
Jojo <joangelvegan@gmail.com>
2016-11-12 17:27:35 -0600

    Add files via upload

---
+++ NameThatCelebrity.java
@@ -0,0 +1,22 @@
+//Author: Joseline Vega
+//Date Created: October 7, 2016
+
+class celebrities{
+  public static void main(String args[]){
+  String s1 = "Allan Alda";
+  String s2 = "John Wanye";
+  String s3 = "Gregory Peck";
+  
+  int allan = s1.length();
+  int john = s2.length();
+  int greg = s3.length();
+  
+  
+  String small1 = s1.substring(2,7);
+  System.out.println(small1);
+  String small2 = s2.substring(2,7);
+  System.out.println(small2);
+  String small3 = s3.substring(2,9);
+  System.out.println(small3);
+  }
+}
