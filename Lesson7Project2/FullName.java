5a44d8da0d0c3d07fc189f8ac73bc76cef2be4da
Jojo <joangelvegan@gmail.com>
2016-11-12 20:31:47 -0600

    wrote program.

--- Lesson7Project2/FullName.java
+++ Lesson7Project2/FullName.java
@@ -1,10 +1,15 @@
+import java.io.*;
+import java.util.*;
+
 class FullName {
     public static void main(String[] args) {
         Scanner firstName = new Scanner(System.in);
         System.out.print("What is your first name? ");
+        String first = firstName.nextLine();
         Scanner lastName = new Scanner(System.in);
-        System.out.print("What is your last name? ")
+        System.out.print("What is your last name? ");
+        String last = lastName.nextLine();
+        String together = first + " " + last;
+        System.out.print("Your full name is " + together + ". ");
     }
-    }
-    
 }
