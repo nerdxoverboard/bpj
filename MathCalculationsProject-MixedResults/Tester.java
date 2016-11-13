26b686bb5fef08e5334183c3ffc550c3a8592d85
Jojo <joangelvegan@gmail.com>
2016-11-12 19:33:57 -0600

    -

--- MathCalculationsProject-MixedResults/Tester.java
+++ MathCalculationsProject-MixedResults/Tester.java
@@ -2,9 +2,19 @@ class Tester{
     public static void main (String[] args){
         double d1 = 37.9;
         double d2 = 1004.128;
-        double d3 = 3
-        double d4 =
+        double d3 = (Math.toRadians(187));
+        double da3 = 3*(Math.PI)*d3;
+        double d4 = (Math.toRadians(122));
+        double da4 = (Math.abs(Math.cos(d4)));
         int i1 = 12;
         int i2 = 18;
+
+        System.out.println("Problem 1: " + (57.2*(i1/i2)+1));
+        System.out.println("Problem 2: " + 57.2*((double)i1/i2)+1);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
     }
 }
