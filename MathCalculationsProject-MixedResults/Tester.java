03dfac887e351d909e8bb1d1dfd8017c20b8cdb4
Jojo <joangelvegan@gmail.com>
2016-11-12 19:56:16 -0600

    rewrote problem 5 output; PEMDAS order was incorrect.

--- MathCalculationsProject-MixedResults/Tester.java
+++ MathCalculationsProject-MixedResults/Tester.java
@@ -14,7 +14,7 @@ class Tester{
         System.out.println("Problem 1: " + (57.2*(i1/i2)+1));
         System.out.println("Problem 2: " + 57.2*((double)i1/i2)+1);
         System.out.println("Problem 3: " + (15-i1*(d1*3)+4));
-        System.out.println("Problem 4: " + (15-i1*(int)(d1*3+4)));
+        System.out.println("Problem 4: " + (15-i1*(int)(d1*3)+4));
         System.out.println("Problem 5: " + (15-i1* ((int)d1*3)+4));
         System.out.println("Problem 6: " + (d3+d4));
         System.out.println("Problem 7: " + (2*(Math.PI)*d4));
