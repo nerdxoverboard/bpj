1be778eab1a58f22b6b6ecc330d7cdcefa3830ef
Jojo <joangelvegan@gmail.com>
2016-11-12 20:22:07 -0600

    Add files via upload

---
+++ FullName.java
@@ -0,0 +1,10 @@
+class FullName {
+    public static void main(String[] args) {
+        Scanner firstName = new Scanner(System.in);
+        System.out.print("What is your first name? ");
+        Scanner lastName = new Scanner(System.in);
+        System.out.print("What is your last name? ")
+    }
+    }
+    
+}
