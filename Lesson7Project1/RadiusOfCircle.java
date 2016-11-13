5e2bf25c53cfd6af730e894b3937bee18e16c004
Jojo <joangelvegan@gmail.com>
2016-11-12 18:55:31 -0600

    -

--- Lesson7Project1/RadiusOfCircle.java
+++ Lesson7Project1/RadiusOfCircle.java
@@ -1,9 +1,11 @@
 //@author nerdxoverboard
 import java.io.*;
 import java.util.*;
-class RadiusOfCircle { 
+class RadiusOfCircle {
     public static void main (String[] args){
         Scanner areaReader = new Scanner(System.in); //reads area input
         System.out.print("What is the area? _");
+        double area = areaReader.nextdouble();
+
     }
 }
